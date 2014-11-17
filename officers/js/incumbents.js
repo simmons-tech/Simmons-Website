3ed57503a44fb0b64ed4d6d76beec4d5ecfd8ac8
Lars Johnson <larsj@mit.edu>
2014-11-16 19:48:34 -0500

    Adding new officers page and diagram

---
+++ officers/js/incumbents.js
@@ -0,0 +1,36 @@
+// Maps offices to people by kerberos.
+
+var incumbents = {
+  // OFFICERS
+  "president" : "larsj",
+  "house_chair" : "cheber",
+  "treasurer" : "kmfarris",
+  "secretary": "woursler",
+
+  "rooming_chair_1" : "orlowski",
+  "rooming_chair_2" : "jujzwang",
+  "history_chair" : "vacant",
+  "social_chair" : "colbertc",
+  "technology_chair" : "adat",
+
+  "athletics_chair" : "cfh119",
+  "frosh_1" : "apark93",
+  "frosh_2" : "nhynes",
+  "publicity_chair" : "cosmosd",
+
+  "facilities_chair" : "dafolabi",
+  "ee_lab_chair" : "erdozain",
+  "workshop_chair" : "adelmore",
+  "library_chair" : "cwetters",
+  "entertainment_chair" : "magallon",
+  "reservations_chair" : "kaugdahl",
+
+  // HOUSE TEAM
+  "housemaster" : "jessig",
+  "associate_housemaster" : "srhall",
+  "rlad" : "jogon_1",
+
+  // OTHER
+  "house_manager" : "nika_h",
+  "desk_captain" : "cfh119",
+}
